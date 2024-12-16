5fc57b3ce7b11652b9b0e371902a3efc60f8328f
pruthvi <pradhanpruthviaditya@gmail.com>
2024-12-16 11:01:21 +0530

    PowerTestClass

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -1,26 +1,26 @@
 package powerpackage;
 
-
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class powertest {
+public class PowerTest {
     @Test
-    public void one_raised_to_one_is_onew() {
-        assert powerfinder.OF(1,1) == 1;
+    public void oneRaisedToOneIsOne() {
+        assertEquals(1, PowerFinder.of(1, 1));
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
-        assert powerfinder.OF(2,1) == 2;
+    public void twoRaisedToOneIsTwo() {
+        assertEquals(2, PowerFinder.of(2, 1));
     }
 
     @Test
-    public void two_power_2_is_4() {
-        assert powerfinder.OF(2, 2) == 4;
+    public void twoPowerTwoIsFour() {
+        assertEquals(4, PowerFinder.of(2, 2));
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
-        assert powerfinder.OF(3, 2) == 3*3;
+    public void threeRaisedToTwoIsNine() {
+        assertEquals(9, PowerFinder.of(3, 2));
     }
 }
